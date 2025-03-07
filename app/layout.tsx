9cdef42674a24123e480ea5dc3664abd944166c0
bonnieace <bonniemasota@gmail.com>
2025-03-07 17:05:40 +0300

    unauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,3 @@
-import {
-  ClerkProvider,
-  RedirectToSignIn,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
 
 import './globals.css'
 import Buttonstyle from './avater'
@@ -17,26 +9,17 @@ export default function RootLayout({
 }) {
   return (
     <div>
-    <ClerkProvider>
       <html lang="en">
         <body className="relative">
           {/* Display SignInButton when the user is signed out */}
-          <SignedOut>
-            <RedirectToSignIn />
-
-          </SignedOut>
-
+         
           {/* Display UserButton when the user is signed in */}
-          <SignedIn>\
-            <UserButton />
-           
-          </SignedIn>
+        
 
           {/* Render the children content */}
           {children}
         </body>
       </html>
-    </ClerkProvider>
     </div>
   )
 }
